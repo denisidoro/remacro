2b807d0f332dceb827f671356f7b901887604bdc
Denis Isidoro <denisidoro@users.noreply.github.com>
2020-07-24 13:38:57 -0300

    Proof of concept (#1)

---
+++ web/src/App.js
@@ -0,0 +1,131 @@
+import React, { useState, useCallback, useMemo } from 'react'
+import './App.css'
+import './prism.css'
+import { transpile } from './core'
+import Editor from 'react-simple-code-editor';
+import { highlight, languages } from 'prismjs/components/prism-core';
+import 'prismjs/components/prism-clike';
+import 'prismjs/components/prism-javascript';
+import 'prismjs/components/prism-markup';
+import 'prismjs/components/prism-jsx';
+import * as ReactMarkdown from 'react-markdown';
+import * as EmojiConvertor from 'emoji-js';
+
+const defaultJs = `const Box = ({ icon, children }) => "> :" + icon + ": " + children
+
+const LoveBox = ({ children }) => <Box icon="heart">{children}</Box>`
+
+const defaultMarkdown = `# Hello World
+
+Lorem ipsum
+
+<LoveBox>This is a custom widget</LoveBox>
+
+| hi | hello |
+| :--: | :--: |
+| foo | bar |
+
+* List Item 1
+  * List Item 1.1
+* List Item 2
+`
+
+const Toggle = ({value, onChange}) => {
+  const handleChange = useCallback(event => {
+    onChange(event.target.checked)
+  }, [onChange])
+
+  return <span style={{fontSize: "small"}} >
+    <input
+      type="checkbox"
+      checked={value}
+      onChange={handleChange} />
+    <span>Render output as markdown</span>
+  </span>
+}
+
+const CodeArea = ({ value, onChange, language }) => {
+  return <div>
+    <Editor
+      className="editor"
+      value={value}
+      onValueChange={onChange}
+      highlight={code => highlight(code, language)}
+      padding={10}
+      style={{
+        fontFamily: '"Fira code", "Fira Mono", monospace',
+        fontSize: 13,
+      }}
+    />
+  </div>
+}
+
+const emoji = new EmojiConvertor();
+
+function safeTranspile(markdown, js) {
+    let c
+    try {
+      c = transpile(markdown, js)
+    } catch (err) {
+      console.log(err)
+      c = markdown
+    }
+    return emoji.replace_colons(c)
+}
+
+const Rendered = ({ markdown, js }) => {
+  const [isMarkdown, setIsMarkdown] = useState(true)
+
+  const content = useMemo(event => {
+    return safeTranspile(markdown, js)
+  }, [markdown, js])
+
+  return <div>
+    <Toggle value={isMarkdown} onChange={setIsMarkdown} />
+    <div className="rendered-markdown markdown-body">
+    { isMarkdown ? <ReactMarkdown source={content} /> : <pre>{content}</pre> }
+  </div>
+</div>
+}
+
+const EditorPane = () => {
+  const [markdown, setMarkdown] = useState(defaultMarkdown)
+  const [js, setJs] = useState(defaultJs)
+
+  return (
+    <div className="flex-container">
+      <div className="flex-item code">
+        <CodeArea value={js} onChange={setJs} language={languages.jsx} />
+        <CodeArea value={markdown} onChange={setMarkdown} language={languages.markup} />
+      </div>
+      <div className="flex-item rendered">
+        <Rendered markdown={markdown} js={js} />
+      </div>
+    </div>
+  );
+}
+
+const Header = () => {
+  return <div className="header-container">
+    <div style={{float: "left"}}>
+      <span><h2>remacro</h2></span>
+      <span><h5>a plain-text macro expander with React-inspired widgets</h5></span>
+    </div>
+    <div style={{float: "right"}}>
+      <span><a href="https://github.com/denisidoro/remacro"><h4>GitHub</h4></a></span>
+    </div>
+  </div>
+}
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Header />
+        <EditorPane />
+      </header>
+    </div>
+  );
+}
+
+export default App;
--- web/src/core/renderer.js
+++ web/src/core/renderer.js
@@ -5,8 +5,8 @@ import { decode } from 'he';
 
 function zip(a, b) {
     var obj = {}
-    for (var i = 0; i < a.length; a++) {
-        obj[a[i]] = b[i]
+    for (var i = 0; i < a.length; i++) {
+            obj[a[i]] = b[i]
     }
     return obj
 }
@@ -19,19 +19,23 @@ function getLowercaseDisplayName(component) {
     return getDisplayName(component).toLowerCase()
 }
 
-export default function myRender(htmlInput, reactComponents) {
+export default function render(markdown, reactComponents) {
     const names = reactComponents.map(getLowercaseDisplayName)
     const components = zip(names, reactComponents)
 
     const opts = {
         transform: function (node, index) {
-            if (node.type !== "tag") return
-            const CurrentComponent = components[node.name]
-            if (!CurrentComponent) return
-            return <CurrentComponent {...node.attribs}>{node.children.map(x => x.data)}</CurrentComponent>
+            if (node.type != "tag") return
+            const C = components[node.name]
+            if (!C) return
+            return React.createElement(
+                C,
+                [node.attribs],
+                [...node.children.map(x => x.data)]
+              )
         }
     }
 
-    const component = ReactHtmlParser(htmlInput, opts)
+    const component = ReactHtmlParser(markdown, opts)
     return decode(renderToStaticMarkup(component).replace(/\\n/g, '\n'))
 }
