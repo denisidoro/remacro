f5f487cf8198a34ed3adef3a6086c41f94f693aa
Logan Connor <loganconnor44@gmail>
2020-07-29 17:01:14 -0400

    giving users the ability to generate the markdown files rather than just viewing in their console

--- cli/cli.js
+++ cli/cli.js
@@ -2,6 +2,7 @@
 'use strict';
 
 const remacro = require('remacro-core');
+const fse = require('fs-extra');
 
 const argv = process.argv;
 
@@ -16,5 +17,26 @@ switch (argv[2]) {
 
 const text = argv[3]
 const macros = argv[5]
-
-console.log(remacro.transpile(text, macros));
+if ( argv[6] !== undefined && argv[6] !== null &&
+		argv[7] !== undefined && argv[7] !== null) {
+	const outputFileName = argv[7];
+	const directory = './remacro-mds';
+	fse.ensureDir(directory).then(() => {
+			console.log(`Generating Remacro Markdonw files at: ${directory}`);
+	
+			fse.writeFile(directory + '/' + outputFileName + '.md', remacro.transpile(text, macros))
+				.then(() => fse.readFile(directory + '/' + outputFileName + '.md', 'utf8'))
+				.then(data => {
+					console.log(`File successfully created!`);
+					console.log(`Output is as followed: \n`);
+					console.log(data);
+				}).catch(err => {
+					console.error(`An error occurred while writing the file: ${err}`);
+			});
+	
+		}).catch(err => {
+			console.error(`An error occurred while ensuring that the directory existed: ${err}`);
+	});
+} else {
+	console.log(remacro.transpile(text, macros));
+}
